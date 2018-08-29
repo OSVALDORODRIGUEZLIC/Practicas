5b01fc848703f529bed9e0c4b5231a5e327f9b31
OSVALDORODRIGUEZLIC <42583740+OSVALDORODRIGUEZLIC@users.noreply.github.com>
2018-08-29 14:41:20 -0500

    f

--- ejemplo-productor-consumidor/src/net/netasystems/granja/Parcela.java
+++ ejemplo-productor-consumidor/src/net/netasystems/granja/Parcela.java
@@ -3,14 +3,29 @@ package net.netasystems.granja;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Parcela.
+ */
 public class Parcela {
 
+	/** The parcela. */
 	private static Queue<Maiz> parcela = new ArrayDeque<>(); //inicializacion de singletons
 	
+	/**
+	 * Adds the maiz.
+	 *
+	 * @param maiz the maiz
+	 */
 	public synchronized static void addMaiz(Maiz maiz) {
 		parcela.add(maiz);
 	}
 	
+	/**
+	 * Gets the maiz.
+	 *
+	 * @return the maiz
+	 */
 	public synchronized static Maiz getMaiz() {
 		if(parcela.isEmpty()) {
 			return null;
