09b0e44f1730f20d65623672e5460e3b6741e61d
OSVALDORODRIGUEZLIC <42583740+OSVALDORODRIGUEZLIC@users.noreply.github.com>
2018-08-29 13:33:56 -0500

    comit

---
+++ ejemplo-productor-consumidor/src/net/netasystems/granja/Agricultor.java
@@ -0,0 +1,35 @@
+package net.netasystems.granja;
+
+public class Agricultor implements Runnable{
+
+	public void siembraOcosecha() {
+		Parcela.addMaiz(new Maiz(getCantidadRandom(), getMaizDescripcion()));
+	}
+	
+	public int getCantidadRandom() {
+		return (int) Math.random() * 100;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getMaizDescripcion() {
+		String tiposMaiz[] = {"TRANSGENICO", "NORMAL", "SIN GLUTEN", "ORGANICO", "BLANCO", "NEGRO", "CON HUITLACOCHE", "PREPARADO", "EN QUESADILLA", "EN QUESADILLA CON QUESO", "MOLIDO"};
+		int index = (int) Math.random() * 10;
+		return tiposMaiz[index];
+	
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			siembraOcosecha();
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				System.out.println("Excepcion de intrerrupcion");
+			}
+		}
+	}
+	
+}
