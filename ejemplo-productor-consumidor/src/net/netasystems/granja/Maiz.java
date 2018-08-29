0770e8080aaa265a3bf1e762969bdef0dee6ce87
OSVALDORODRIGUEZLIC <42583740+OSVALDORODRIGUEZLIC@users.noreply.github.com>
2018-08-29 14:41:00 -0500

    d

--- ejemplo-productor-consumidor/src/net/netasystems/granja/Maiz.java
+++ ejemplo-productor-consumidor/src/net/netasystems/granja/Maiz.java
@@ -2,38 +2,77 @@ package net.netasystems.granja;
 
 import java.io.Serializable;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Maiz.
+ */
 public class Maiz implements Serializable {
 
+	/** The cantidad. */
 	private Integer cantidad;
+	
+	/** The descripcion. */
 	private String descripcion;
 
+	/**
+	 * Instantiates a new maiz.
+	 *
+	 * @param cantidad the cantidad
+	 * @param descripcion the descripcion
+	 */
 	public Maiz(Integer cantidad, String descripcion) {
 		super();
 		this.cantidad = cantidad;
 		this.descripcion = descripcion;
 	}
 
+	/**
+	 * Gets the cantidad.
+	 *
+	 * @return the cantidad
+	 */
 	public Integer getCantidad() {
 		return cantidad;
 	}
 
+	/**
+	 * Sets the cantidad.
+	 *
+	 * @param cantidad the new cantidad
+	 */
 	public void setCantidad(Integer cantidad) {
 		this.cantidad = cantidad;
 	}
 
+	/**
+	 * Gets the descripcion.
+	 *
+	 * @return the descripcion
+	 */
 	public String getDescripcion() {
 		return descripcion;
 	}
 
+	/**
+	 * Sets the descripcion.
+	 *
+	 * @param descripcion the new descripcion
+	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "Maiz [cantidad=" + cantidad + ", descripcion=" + descripcion + "]";
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -43,6 +82,9 @@ public class Maiz implements Serializable {
 		return result;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
